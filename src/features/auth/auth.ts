96ad39b0728ac683ec62ee9d9b245d15c8dfdcc3
Baker Bokorney <bbokorney@gmail.com>
2022-05-15 11:26:53 -0600

    Clean up debug printing

--- src/features/auth/auth.ts
+++ src/features/auth/auth.ts
@@ -8,8 +8,6 @@ import {
 import { updateUser } from "./authSlice";
 import { store } from "../../app/store";
 
-console.log("Setting up auth");
-
 const { dispatch } = store;
 
 const auth = getAuth();
@@ -17,11 +15,9 @@ const provider = new GoogleAuthProvider();
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    console.log("Logged in, according to state change");
     const { uid, email, displayName } = user;
     dispatch(updateUser({ id: uid, email, displayName }));
   } else {
-    console.log("Not logged in, according to state change");
     dispatch(updateUser(null));
   }
 });
@@ -31,9 +27,7 @@ export const signUserIn = () => {
 };
 
 export const signUserOut = () => {
-  signOut(auth).then(() => {
-    console.log("Sign-out successful");
-  }).catch((error) => {
+  signOut(auth).catch((error) => {
     console.log("Sign-out error:", error);
   });
 };
