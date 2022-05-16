b4543d4330f8781056249eefaf2b2e93d92877a3
Baker Bokorney <bbokorney@gmail.com>
2022-05-15 20:07:32 -0600

    Add menu from floating add button

--- src/ui/layout/FloatingAddButton.tsx
+++ src/ui/layout/FloatingAddButton.tsx
@@ -1,14 +1,60 @@
+import { useState, MouseEvent } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
+import Box from "@mui/material/Box";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
 
-const FloatingAddButton = () => (
-  <Fab
-    color="primary"
-    sx={{
-      position: "fixed", bottom: "70px", right: "15px",
-    }}
-  >
-    <AddIcon />
-  </Fab>
-);
+const FloatingAddButton = () => {
+  const elem = <ActivityTypeMenu />;
+  return (
+    <Box
+      sx={{
+        position: "fixed", bottom: "70px", right: "15px",
+      }}
+    >
+      {elem}
+    </Box>
+
+  );
+};
 export default FloatingAddButton;
+
+const ActivityTypeMenu = () => {
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (destination: string) => {
+    navigate(destination);
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <Fab color="primary" onClick={handleClick}>
+        <AddIcon />
+      </Fab>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={() => handleClose("/activities/add")}>Core</MenuItem>
+        <MenuItem onClick={() => handleClose("/activities/add")}>Strength + antagonist</MenuItem>
+        <MenuItem onClick={() => handleClose("/activities/add")}>Day hike</MenuItem>
+      </Menu>
+    </>
+  );
+};
